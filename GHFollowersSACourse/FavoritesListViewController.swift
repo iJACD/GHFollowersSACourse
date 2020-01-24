22db36e8f27edb37c6fc77b326ca3ff55c368612
JohnAnthony Dobson <jcdobson2@gmail.com>
2020-01-24 13:04:48 -0500

    completed getting started video, added assets and setup navigation. save.

--- GHFollowersSACourse/FavoritesListViewController.swift
+++ GHFollowersSACourse/FavoritesListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FavoritesListViewController.swift
 //  GHFollowersSACourse
 //
 //  Created by JohnAnthony on 1/24/20.
@@ -8,13 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class FavoritesListViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBlue
     }
-
-
 }
-
---
+++ GHFollowersSACourse/SearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchViewController.swift
+//  GHFollowersSACourse
+//
+//  Created by JohnAnthony on 1/24/20.
+//  Copyright © 2020 iJACD. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPurple
+    }
+}
