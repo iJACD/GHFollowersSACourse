46f8e7b9bd5dd2483be035d20634fe71139464e9
JohnAnthony Dobson <jcdobson2@gmail.com>
2020-01-29 14:44:05 -0500

    made follower and user models and fixed a few bugs.

---
+++ GHFollowersSACourse/Models/Follower.swift
@@ -0,0 +1,14 @@
+//
+//  Follower.swift
+//  GHFollowersSACourse
+//
+//  Created by JohnAnthony on 1/29/20.
+//  Copyright © 2020 iJACD. All rights reserved.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
